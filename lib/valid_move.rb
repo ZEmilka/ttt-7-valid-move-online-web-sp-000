ecb3534d77c7c0fd76a55e7b840bde3ca1b8be09
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-31 21:22:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,14 +6,5 @@ def position_taken?(board,index)
   end
 end
 def valid_move?(board,index)
-  #if position_taken?==false
-  #  return true
-  #elsif position_taken?==true && position_taken?==nil
-  #  return false
-  #end
-  if board[index]==" "||board[index]==""||board[index]==nil
-    return true
-  else #board[index].between?(1,9)=="X||0"
-    return false||nil
-  end
+index.between?(0,8) && position_taken?(board,index)
 end
