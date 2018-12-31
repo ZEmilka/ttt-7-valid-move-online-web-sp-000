e706330dc9b0e3412bb5ac5f113dc33bc8d95b78
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-31 20:49:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,5 +14,5 @@ def valid_move?(board,index)
   if board[index]==" "||board[index]==""||board[index]==nil
     return true
   else #board[index].between?(1,9)=="X||0"
-    return false
+    return false||nil
 end
