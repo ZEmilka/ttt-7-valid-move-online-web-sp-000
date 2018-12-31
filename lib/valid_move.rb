76ccdd37500e83536bf81cf6b6cdf420669a4c74
Emilia Ziemichód <emifrompoland@gmail.com>
2018-12-31 19:42:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,inex)
+  if board[index]==" "||board[index]==""||board[index]==nil
+    return false
+  else
+    return true
+  end
+end
